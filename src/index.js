d49354c035150792f2b9dd43e18a253bff33e3a4
Selin <selinozturk@Selin-MacBook-Pro.local>
2020-07-08 13:10:42 +0300

    States implemented

--- src/index.js
+++ src/index.js
@@ -2,13 +2,33 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // the only "direct assignment"
+    this.state = { lat: null, errorMessage: "" };
+
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({
+          lat: position.coords.latitude,
+        });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
   // React says we have to define render()!!!
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      (position) => console.log(position),
-      (err) => console.log(err)
-    );
-    return <div>Latitude: </div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+    if (!this.state.errorMessage && this.state.lat) {
+      return <div>Latitute: {this.state.lat}</div>;
+    }
+    return <div>Loading...</div>;
   }
 }
 
